b6d721bd9ac011a712f2a77476b4948f0c448664
Max Witalec <mwitalec01@gmail.com>
2023-12-13 22:22:20 +0000

    added modal to driver + team

--- obj/Debug/net8.0/CA3F1.AssemblyInfo.cs
+++ obj/Debug/net8.0/CA3F1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CA3F1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a8aadb6242681f1780f148f68062ae9f6f4ebc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb662574b5c50ab02825e4ae944c5fc00cd1eb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CA3F1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CA3F1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
